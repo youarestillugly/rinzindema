315b4e2bcf183f3b1b76ec08d8c47839c08fb99e
Rinzin <justlittletoxic@gmail.com>
2024-11-23 01:07:25 +0600

    Add files via upload

---
+++ about.js
@@ -0,0 +1,15 @@
+// Function to toggle dropdown visibility
+function toggleDropdown() {
+    const dropdownContent = document.getElementById("dropdown-content");
+    dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            dropdowns[i].style.display = "none";
+        }
+    }
+}
